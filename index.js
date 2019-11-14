591ba58e5127710509c8e13a3b990f4073e8d8b1
Jonathan <56617776+qb1968@users.noreply.github.com>
2019-11-14 18:54:15 -0500

    so far

--- index.js
+++ index.js
@@ -41,7 +41,24 @@ class Airplane {
 */
 
 class Person {
-
+constructor(name,age){
+this.name = name,
+this.age = age,
+this.stomach = []
+};
+eat(someFood) {
+  if(this.stomach.length < 10){
+     this.stomach.push(someFood);
+ }
+ }
+ poop() {
+  if(this.stomach.length >= 10){
+    this.stomach.length = 0;
+ }
+}
+toString() {
+  return `${this.name} and${this.age}`;
+}
 }
 
 /*
@@ -59,8 +76,16 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
 
+  }
 }
+
+
 
 /*
   TASK 3
@@ -75,6 +100,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes){
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location;
+  };
+  speak(){
+    return `Hello my name is ${this.name}, Iam from ${this.location}`
+  }
 
 }
 
@@ -92,7 +125,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty,
+    this.favLanguage = attributes.favLanguage,
+    this.catchPhrase = attributes.catchPhrase
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject) {
+   return  `${student.name} receives a perfect score on ${subject}`
+  }
 
 }
 
@@ -111,7 +156,19 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+   this. previousBackground = attributes.previousBackground,
+    this.className = attributes.className,
+    this.favSubjects = attributes.favSubjects
+  }
+  listSubjects() {
+    return `Loving ${this.favSubjects}!`
+  }
+  PRAssignment(subject) {
+    
+  }
 
 }
 
